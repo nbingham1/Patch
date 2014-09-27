70b59c3299e1efdf6224a665e2fca082b17469bb
nak54 <nak54@cornell.edu>
2014-09-27 02:12:43 -0400

    rhine

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import nltk
+import math
 from textblob import *
 from textblob.wordnet import *
 from nltk.corpus import wordnet as wn
@@ -6,6 +7,15 @@ from textProc import *
 
 from rhine import *
 from rhine_reader import *
+
+
+rb = RhineBundle()
+rb.rhineGenerate('Rhine.txt')
+#To get a fresh rhine request, do rb.freshRhine() which returns a rhine
+
+def diff(a, b):
+    b = set(b)
+    return [aa for aa in a if aa not in b]
 
 def wordnet_pos(treebank_pos):
 	if "NN" in treebank_pos:
@@ -61,7 +71,44 @@ def list_path_similarity(words1, words2):
 				covered2.append(pair[3])
 
 	remove.sort(reverse=True)
+	for idx in remove:
+		pairs.pop(idx)
 
+	total=0
+	score=0
+	for pair in pairs:
+		score += pair[0]*pair[1]
+		total += pair[1]
+	
+	score /= total	
+
+	return score
+
+def rhine_similarity(words1, words2):
+	pairs = []
+	for word1 in words1:
+		for word2 in words2:
+                        print word1[0]
+                        print word2[0]
+                        dist = rb.freshRhine().distance(word1[0],word2[0])
+                        if math.isnan(dist):
+                            dist = 100
+                        dist = dist/100
+			pairs.append([dist, min(word1[1], word2[1]), word1[0], word2[0]])
+	pairs.sort(reverse=True)
+	covered1 = []
+	covered2 = []
+	remove = []
+	for index, pair in enumerate(pairs):
+		if pair[2] in covered1 and pair[3] in covered2:
+			remove.append(index)
+		else:
+			if pair[2] not in covered1:
+				covered1.append(pair[2])
+			if pair[3] not in covered2:
+				covered2.append(pair[3])
+
+	remove.sort(reverse=True)
 	for idx in remove:
 		pairs.pop(idx)
 
@@ -76,10 +123,9 @@ def list_path_similarity(words1, words2):
 	return score
 
 
-rb = RhineBundle()
-rb.rhineGenerate('Rhine.txt')
-#To get a fresh rhine request, do rb.freshRhine() which returns a rhine
+print rb.freshRhine().distance('Warren', 'Iran')
 
+# Pull out news articles
 f1 = open('./data/articleFox.txt')
 a = f1.read()
 text1 = TextBlob(a)
@@ -88,17 +134,37 @@ f2 = open('./data/article2.txt')
 b = f2.read()
 text2 = TextBlob(b)
 
-wordlist1 = extract_words(text1, "NN")
-freq1 = freqGetTuple(50, wordlist1)
-print freq1
-synsets1 = tuples_to_synsets(freq1, "NN")
+######
 
-wordlist2 = extract_words(text2, "NN")
-freq2 = freqGetTuple(50, wordlist2)
-print freq2
-synsets2 = tuples_to_synsets(freq2, "NN")
+wl1_nn = extract_words(text1, "NN")
+wl1_nnp = extract_words(text1, "NNP")
+diff(wl1_nn,wl1_nnp)
 
+freq1_nn = freqGetTuple(50, wl1_nn)
+print freq1_nn
+synsets1 = tuples_to_synsets(freq1_nn, "NN")
+
+freq1_nnp = freqGetTuple(5, wl1_nnp)
+print freq1_nnp
+
+#######
+
+wl2_nn = extract_words(text2, "NN")
+wl2_nnp = extract_words(text2, "NNP")
+diff(wl2_nn,wl2_nnp)
+
+freq2_nn = freqGetTuple(50, wl2_nn)
+print freq2_nn
+synsets2 = tuples_to_synsets(freq2_nn, "NN")
+
+freq2_nnp = freqGetTuple(5, wl2_nnp)
+print freq1_nnp
+
+print "list_path_similarity of the two articles: "
 print list_path_similarity(synsets1, synsets2)
+
+print "pronoun similarity of the two articles: "
+print rhine_similarity(freq1_nnp, freq2_nnp)
 
 #print extract_words(wiki, "NN").path_similarity(extract_words(wiki2, "NN"))
 

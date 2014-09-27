bb75ec33c34d04b15bba1cc883840b3c22c2b468
Ned Bingham <eab242@cornell.edu>
2014-09-27 19:07:57 -0400

    fixed argument polling

--- ui/cgi/pull_issue_summaries.py
+++ ui/cgi/pull_issue_summaries.py
@@ -19,6 +19,6 @@ print
 
 # data
 for result in results:
-	print '<div class="item" id="item_' + str(result[0]) + '" onclick="$(\'#account_pane\').hide(); $(\'#issue_pane\').show(); pull_argument_summaries(\'' + str(result[0]) + '\', function(data, response){ $(\'#issues_pane\').append(data); });">'
+	print '<div class="item" id="item_' + str(result[0]) + '" onclick="$(\'#account_pane\').hide(); $(\'#issue_pane\').show(); pull_argument_summaries(\'' + str(result[0]) + '\', function(data, response){ $(\'#issue_pane\').append(data); });">'
 	print result[1]
 	print '</div>'

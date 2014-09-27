c050fce6a1694931dda1af33f5e6d87cf2dc8744
Ned Bingham <eab242@cornell.edu>
2014-09-26 19:25:28 -0700

    created function to extract nouns from a blob

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+import nltk
 from textblob import Word
 from textblob.wordnet import VERB
-word = Word("octopus")
-print word.synsets
-print Word("hack").get_synsets(pos=VERB)
+from textblob import TextBlob
+
+
+def extract_nouns(blob):
+	nouns = []
+	tags = blob.tags
+	for tag in tags:
+		if "NN" in tag[1]:
+			nouns.append(tag[0])
+	return nouns
+
+wiki = TextBlob("The dog ran around the flag pole.")
+wiki2 = TextBlob("Dogs like poles, but not flags.")
+
+print extract_nouns(wiki)
+
